ede392f0a8ea331123599d5b694a28c7f1a3a92e
shivani-ankam <shivaniankam1024@gmail.com>
2024-10-23 22:47:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Welcome to FLM - Deploying React Application using AWS Amplify
+          Edit <code>src/App.js</code> Hi, I am Ironman- Deploying React Application using AWS Amplify
         </p>
         <a
           className="App-link"
